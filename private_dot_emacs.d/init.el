de73c33a478c99b5911aa40e766b6b8620a8f5a0
Samuel Bonney <github.grinning329@silomails.com>
2024-12-06 01:22:57 +0000

    Add ob-yaml

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -384,6 +384,10 @@
       (concat "${title:*} "
               (propertize "${tags:10}" 'face 'org-tag)))
 
+(use-package ob-yaml
+  :vc (ob-yaml :url "https://github.com/llhotka/ob-yaml"
+		       :branch "main"))
+
 (use-package nov
   :init
   (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
