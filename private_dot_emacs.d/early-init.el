c15d01ef46741fe7be35c6f2e73656731861d58e
Samuel Bonney <github.grinning329@silomails.com>
2024-12-17 21:51:19 +0000

    Update ssh-sock path

--- private_dot_emacs.d/early-init.el
+++ private_dot_emacs.d/early-init.el
@@ -6,7 +6,7 @@
 
    ((string-equal system-type "android")
     (let ((termux-bin "/data/data/com.termux/files/usr/bin")
-          (ssh-sock "/data/data/com.termux/files/home/.ssh/ssh_auth_sock"))
+          (ssh-sock "/data/data/com.termux/files/home/.ssh/ssh-agent.sock"))
       (setenv "PATH" (format "%s:%s" termux-bin (getenv "PATH")))
       (add-to-list 'exec-path termux-bin)
       (setenv "SSH_AUTH_SOCK" ssh-sock)))))
