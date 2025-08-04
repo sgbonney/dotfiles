a82440f35319d7319f73f2ba7d64e21c563093c8
Samuel Bonney <github.grinning329@silomails.com>
2025-08-04 22:36:33 +0100

    Use .bashrc

--- private_executable_launch_keepassxc.sh
+++ private_executable_launch_keepassxc.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ -z "$SSH_AUTH_SOCK" ] || [ ! -S "$SSH_AUTH_SOCK" ]; then
-    rm -f ~/.ssh/ssh-agent.sock
-    eval "$(ssh-agent -s -a ~/.ssh/ssh-agent.sock)"
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh-agent.sock
-
+eval "$(cat .bashrc)"
 termux-x11 :1 -xstartup "keepassxc" &
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
