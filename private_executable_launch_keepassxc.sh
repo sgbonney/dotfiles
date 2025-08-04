10f03907ad3b79033165ee57fbeaa85840e23fd1
Samuel Bonney <github.grinning329@silomails.com>
2025-08-04 22:16:32 +0100

    Remove comments

--- private_executable_launch_keepassxc.sh
+++ private_executable_launch_keepassxc.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-# Start SSH agent if not already running
 if [ -z "$SSH_AUTH_SOCK" ] || [ ! -S "$SSH_AUTH_SOCK" ]; then
-    # Remove the old socket file if it exists
     rm -f ~/.ssh/ssh-agent.sock
     eval "$(ssh-agent -s -a ~/.ssh/ssh-agent.sock)"
 fi
-
-# Set SSH_AUTH_SOCK to the fixed socket location
 export SSH_AUTH_SOCK=~/.ssh/ssh-agent.sock
 
 termux-x11 :1 -xstartup "keepassxc" &
