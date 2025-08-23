013189cea8ea53318c606950b8361349c2b5c1b1
Samuel Bonney <github.grinning329@silomails.com>
2025-08-23 08:09:40 +0100

    Add universal installation script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+handle_failure() {
+    echo "Failed to install $1"
+    exit 1
+}
+
+if [[ "$OSTYPE" == *"android"* ]]; then
+    pkg install -y git || handle_failure "Git"
+    pkg install -y chezmoi || handle_failure "Chezmoi"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_failure "Homebrew"
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    brew install chezmoi || handle_failure "Chezmoi"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
+
+chezmoi "$@"
