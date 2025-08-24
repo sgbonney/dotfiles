c6f55a196cd5a6a98f38b0be3f777933b1aaaf85
Samuel Bonney <github.grinning329@silomails.com>
2025-08-25 00:27:38 +0100

    Update handle_failure function

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 handle_failure() {
-    echo "Failed to install $1"
-    exit 1
+    if ! command -v "$1" &> /dev/null; then
+        echo "Failed to install $1."
+        exit 1
+    fi
 }
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_failure "Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_failure "homebrew"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     brew install chezmoi
+    handle_failure "chezmoi"
 elif [[ "$OSTYPE" == *"android"* ]]; then
-    pkg install -y git || handle_failure "Git"
-    pkg install -y chezmoi || handle_failure "chezmoi"
+    pkg install -y git
+    handle_failure "git"
+    pkg install -y chezmoi
+    handle_failure "chezmoi"
 else
     echo "Unsupported operating system: $OSTYPE"
     exit 1
