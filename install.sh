d2737c33feb654e27162158fce4ba26804b26212
Samuel Bonney <github.grinning329@silomails.com>
2025-08-24 15:23:39 +0100

    Move special parameter into if/else

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_failure "Homebrew"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     brew install chezmoi || handle_failure "chezmoi"
+    chezmoi "$@"
 elif [[ "$OSTYPE" == *"android"* ]]; then
     pkg install -y git || handle_failure "Git"
     pkg install -y chezmoi || handle_failure "chezmoi"
+    chezmoi "$@"
 else
     echo "Unsupported operating system: $OSTYPE"
     exit 1
 fi
-
-chezmoi "$@"
