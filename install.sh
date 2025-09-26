d26983a7ea4c5ee186f36cdbc3e8b446627d6043
Samuel Bonney <github.isotope341@passmail.net>
2025-09-26 21:21:12 +0100

    Revert "Add apt update/upgrade"
    This reverts commit be5911795fc7f907962e131ff21b52af5736b5d3.

--- install.sh
+++ install.sh
@@ -12,8 +12,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && brew install chezmoi
     handle_failure "chezmoi"
 elif [[ "$OSTYPE" == *"android"* ]]; then
-    apt update -y && apt full-upgrade -y -o Dpkg::Options::="--force-confnew"
-    pkg update -y && pkg upgrade -y
+    pkg update -y || { echo "Failed to update package list. Exiting."; exit 1; }
     pkg install -y git
     handle_failure "git"
     pkg install -y chezmoi
