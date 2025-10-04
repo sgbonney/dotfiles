e228eb886e4ce0261a0798e29c95dce1f2770f77
Samuel Bonney <github.isotope341@passmail.net>
2025-10-04 12:15:12 +0100

    Add passwd check

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ handle_failure() {
 }
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    [ "$(passwd -S | awk '{print $2}')" == "NP" ] && passwd
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || handle_failure "brew"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && brew install chezmoi
     handle_failure "chezmoi"
